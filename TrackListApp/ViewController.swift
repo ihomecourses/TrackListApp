b8e96751fbcb5db91446173f2b85c9230461dcd9
ihomecourses <ihomecourses@gmail.com>
2021-04-14 18:25:10 +0300

    2. Table View V2

--- TrackListApp/ViewController.swift
+++ TrackListApp/ViewController.swift
@@ -7,17 +7,17 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITableViewDataSource {
+class ViewController: UITableViewController {
   
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 10
     }
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
         cell.textLabel?.text = "Cell index \(indexPath.row)"
         return cell
